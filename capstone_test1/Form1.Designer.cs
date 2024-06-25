523b7d06d76ba8e201d2ad76504bcd649c813d19
PROGRAMMER_PARK <116262231+Lupak442@users.noreply.github.com>
2024-06-25 18:05:44 +0900

    Add files via upload

---
+++ capstone_test1/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace capstone_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hScrollBar_min = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar_max = new System.Windows.Forms.HScrollBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.circle_max_value = new System.Windows.Forms.TextBox();
+            this.circle_min_value = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(891, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "영상시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(138, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(138, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // hScrollBar_min
+            // 
+            this.hScrollBar_min.Location = new System.Drawing.Point(167, 88);
+            this.hScrollBar_min.Name = "hScrollBar_min";
+            this.hScrollBar_min.Size = new System.Drawing.Size(401, 35);
+            this.hScrollBar_min.TabIndex = 4;
+            this.hScrollBar_min.Value = 11;
+            // 
+            // hScrollBar_max
+            // 
+            this.hScrollBar_max.Location = new System.Drawing.Point(167, 248);
+            this.hScrollBar_max.Maximum = 150;
+            this.hScrollBar_max.Name = "hScrollBar_max";
+            this.hScrollBar_max.Size = new System.Drawing.Size(401, 35);
+            this.hScrollBar_max.TabIndex = 4;
+            this.hScrollBar_max.Value = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "고유 번호";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "주사위 눈금";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "원 최소판별크기";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "원 최대 판별크기";
+            // 
+            // circle_max_value
+            // 
+            this.circle_max_value.Location = new System.Drawing.Point(38, 276);
+            this.circle_max_value.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.circle_max_value.Name = "circle_max_value";
+            this.circle_max_value.Size = new System.Drawing.Size(79, 25);
+            this.circle_max_value.TabIndex = 7;
+            // 
+            // circle_min_value
+            // 
+            this.circle_min_value.Location = new System.Drawing.Point(38, 124);
+            this.circle_min_value.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.circle_min_value.Name = "circle_min_value";
+            this.circle_min_value.Size = new System.Drawing.Size(79, 25);
+            this.circle_min_value.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::capstone_test1.Properties.Resources.pngegg__2_;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(581, 575);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 110);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "모터 동작";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(41, 380);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(285, 286);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "눈금 및 고유번호";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 31);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 25);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "130";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 64);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 25);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "255";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(20, 97);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 25);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::capstone_test1.Properties.Resources.pngegg__1_;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(900, 594);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 91);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "데이터 저장";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(943, 176);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(113, 25);
+            this.textBox4.TabIndex = 14;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Location = new System.Drawing.Point(719, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(166, 145);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "이진화 임계값 설정";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::capstone_test1.Properties.Resources.pngegg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1138, 765);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.circle_min_value);
+            this.Controls.Add(this.circle_max_value);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.hScrollBar_max);
+            this.Controls.Add(this.hScrollBar_min);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.HScrollBar hScrollBar_min;
+        private System.Windows.Forms.HScrollBar hScrollBar_max;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox circle_max_value;
+        private System.Windows.Forms.TextBox circle_min_value;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ capstone_test1/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using Point = OpenCvSharp.Point;
+using Tesseract;
+using IronOcr;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+
+namespace capstone_test1
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        int n;
+        private static System.Threading.Timer timer;
+
+        public void Run()
+        {
+            VideoCapture video = new VideoCapture(0);
+
+            Mat frame = new Mat();
+            Mat gray = new Mat();
+            Mat gaussian_blur = new Mat();
+            Mat image_canny = new Mat();
+            Mat binary = new Mat();
+            Mat cut = new Mat();
+            Bitmap bitmap;
+            while (Cv2.WaitKey(33) != 'q')
+            {
+
+                video.Read(frame);
+
+                Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+                Cv2.GaussianBlur(gray, gaussian_blur, new OpenCvSharp.Size(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox3.Text)), 1, 1, BorderTypes.Default);
+                Cv2.Threshold(gaussian_blur, binary, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), ThresholdTypes.Binary);
+                cut = binary.SubMat(new OpenCvSharp.Rect(300, 300, 150, 100));
+                bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(cut);
+                
+
+                var Ocr = new IronTesseract();
+                var Input = new OcrInput(bitmap);
+                
+                    // Input.Deskew();  // use if image not straight
+                    // Input.DeNoise(); // use if image contains digital noise
+                    var result = Ocr.Read(Input);
+                    //Console.WriteLine(result.Text);
+                
+                //Pix pix = PixConverter.ToPix(bitmap);
+                //var engine = new TesseractEngine(@"C:\Users\user\source\repos\capstone_test1\capstone_test1\bin\Debug\tessdata", "eng", EngineMode.TesseractAndLstm);
+                //var Result = engine.Process(pix);
+                //Result.GetText();
+
+                CircleSegment[] circles = Cv2.HoughCircles(gaussian_blur, HoughModes.Gradient, 1,
+                         gaussian_blur.Rows / 20,  // change this value to detect circles with different distances to each other
+                         150, 25, hScrollBar_min.Value, hScrollBar_max.Value // change the last two parameters
+                                                                             // (min_radius & max_radius) to detect larger circles
+                );
+                n = circles.Length;
+                for (int i = 0; i < circles.Length; i++)
+                {
+                    Point2f center = circles[i].Center;
+                    Cv2.Circle(frame, (int)center.X, (int)center.Y, 1, new Scalar(0, 100, 100), 3, LineTypes.AntiAlias);
+                    // circle outline                    
+                    Cv2.Circle(frame, (int)center.X, (int)center.Y, (int)circles[i].Radius, new Scalar(255, 0, 255), 3, LineTypes.AntiAlias);
+                }
+
+                /*Cv2.Canny(gaussian_blur, image_canny, 100, 200, 3, true);
+                Mat element = Cv2.GetStructuringElement(MorphShapes.Ellipse, new OpenCvSharp.Size(7, 7));
+                Mat morph = new Mat();
+                Cv2.MorphologyEx(image_canny, morph, MorphTypes.Close, element, iterations: 1);
+                //Cv2.ImShow("mor", morph);
+                ulong idx, ii;
+                Point[][] contours;
+
+                HierarchyIndex[] hierarchy;
+                Cv2.FindContours(morph, out contours, out hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxTC89KCOS);
+
+                Mat contours_img = new Mat();
+                Cv2.CvtColor(image_canny, contours_img, ColorConversionCodes.GRAY2BGR);
+                for (idx = 0; idx < Convert.ToUInt32(contours.Length); idx++)
+                {
+                    RotatedRect rect = Cv2.MinAreaRect(contours[idx]);
+                    double areaRatio = Math.Abs(Cv2.ContourArea(contours[idx]) / (rect.Size.Width * rect.Size.Height));
+                    Cv2.DrawContours(contours_img, contours, Convert.ToInt32(idx), Scalar.Red, 2);
+
+                }*/
+                circle_min_value.Text = Convert.ToString(hScrollBar_min.Value);
+                circle_max_value.Text = Convert.ToString(hScrollBar_max.Value);
+                label1.Text = Convert.ToString(n);
+                //label2.Text = result.GetText();
+                label2.Text = result.Text;
+                //label7.Text = Result.GetText();
+                //Cv2.ImShow("con", contours_img);
+                Cv2.ImShow("cut", cut);
+                Cv2.ImShow("frame", frame);
+                
+            }
+            frame.Dispose();
+            video.Release();
+            Cv2.WaitKey(0);
+        }
+
+            public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e) //영상시작
+        {
+            Run();
+
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        int count = 0;
+
+
+        string id = null;
+        string one_c = null;
+        string two_c = null;
+        string three_c = null;
+        string four_c = null;
+        string five_c = null;
+        string six_c = null;
+        private void button3_Click(object sender, EventArgs e) //눈금인식 끄기
+        {
+            
+            if (count/6 == 0)
+            {
+                id = label2.Text;
+            }
+            if(count%6 == 0)
+            {
+                one_c = label1.Text;
+            }
+            if (count % 6 == 1)
+            {
+                two_c = label1.Text;
+            }
+            if (count % 6 == 2)
+            {
+                three_c = label1.Text;
+            }
+            if (count % 6 == 3)
+            {
+                four_c = label1.Text;
+            }
+            if (count % 6 == 4)
+            {
+                five_c = label1.Text;
+            }
+            if (count % 6 == 5)
+            {
+                six_c = label1.Text;
+                dogs.Add(new Dog() { id = label2.Text, one = one_c, two = two_c, thr = three_c, four = four_c, five = five_c, six = six_c });
+                count = 0;
+            }
+
+
+            textBox4.Text = Convert.ToString(count);
+            count += 1;
+            
+
+        }
+        List<Dog> dogs = new List<Dog>();
+        private void button2_Click(object sender, EventArgs e) //데이터 저장
+        {
+             Excel.Application excelApp = null;
+             Excel.Workbook workBook = null;
+             Excel.Worksheet workSheet = null;
+
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);  // 바탕화면 경로
+                string path = Path.Combine(desktopPath, "주사위판별검사데이터.xlsx");                              // 엑셀 파일 저장 경로
+
+                excelApp = new Excel.Application();                             // 엑셀 어플리케이션 생성
+                workBook = excelApp.Workbooks.Add();                            // 워크북 추가
+                workSheet = workBook.Worksheets.get_Item(1) as Excel.Worksheet; // 엑셀 첫번째 워크시트 가져오기
+
+                workSheet.Cells[1, 1] = "고유번호";
+                workSheet.Cells[1, 2] = "1";
+                workSheet.Cells[1, 3] = "2";
+                workSheet.Cells[1, 4] = "3";
+                workSheet.Cells[1, 5] = "4";
+                workSheet.Cells[1, 6] = "5";
+                workSheet.Cells[1, 7] = "6";
+
+                // 엑셀에 저장할 개 데이터
+                
+
+                
+                for (int i = 0; i < dogs.Count; i++)
+                {
+                    Dog dog = dogs[i];
+
+                    // 셀에 데이터 입력
+                    workSheet.Cells[2 + i, 1] = dog.id;
+                    workSheet.Cells[2 + i, 2] = dog.one;
+                    workSheet.Cells[2 + i, 3] = dog.two;
+                    workSheet.Cells[2 + i, 4] = dog.thr;
+                    workSheet.Cells[2 + i, 5] = dog.four;
+                    workSheet.Cells[2 + i, 6] = dog.five;
+                    workSheet.Cells[2 + i, 7] = dog.six;
+                    workSheet.Cells[2 + i, 8] = dog.one+"+"+dog.thr;
+                    workSheet.Cells[2 + i, 9] = dog.two + "+" + dog.four;
+                    workSheet.Cells[2 + i, 10] = dog.five + "+" + dog.six;
+
+                }
+
+                workSheet.Columns.AutoFit();                                    // 열 너비 자동 맞춤
+                workBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookDefault);    // 엑셀 파일 저장
+                workBook.Close(true);
+                excelApp.Quit();
+            }
+            finally
+            {
+                ReleaseObject(workSheet);
+                ReleaseObject(workBook);
+                ReleaseObject(excelApp);
+            }
+
+
+
+        }
+        static void ReleaseObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);  // 액셀 객체 해제
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect();   // 가비지 수집
+            }
+        }
+
+
+
+    }
+    class Dog
+    {
+        public string id;
+        public string one;     // 개 이름
+        public string two;   // 개 종류
+        public string thr;      // 개 성별
+        public string four;
+        public string five;
+        public string six;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
